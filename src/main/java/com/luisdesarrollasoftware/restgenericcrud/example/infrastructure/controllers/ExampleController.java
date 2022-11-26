1430823ab27ba323470503b947d90881ca5ec4d3
PCSOBREMESA\lmgomez <luisdesarrollasoftware@gmail.com>
2022-11-26 13:08:56 +0100

    Basic Api-rest CRUD with generic code. Contains an example to extends generic code.

--- src/main/java/com/luisdesarrollasoftware/restgenericcrud/example/infrastructure/controllers/ExampleController.java
+++ src/main/java/com/luisdesarrollasoftware/restgenericcrud/example/infrastructure/controllers/ExampleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping(path = "/v1/autor")
+@RequestMapping(path = "/v1/example")
 public class ExampleController extends BaseControllerImpl<Example, ExampleServiceImpl> {
 
 }
